04f005d5423b083e7484abd7aae820b861506358
Alex Clark <aclark@aclark.net>
2021-10-10 09:18:38 -0400

    Update

--- logo.py
+++ logo.py
@@ -14,31 +14,39 @@ def create_image(filename):
     """
     Create image
     """
-    x = 1400
-    y = 1400
+    x = 400
+    y = 400
+
+    text = "$ make …"
 
     # bg white
-    # out = Image.new("RGB", (x, y), (255, 255, 255))
+    out = Image.new("RGB", (x, y), (255, 255, 255))
 
     # bg black
-    out = Image.new("RGB", (x, y), (0, 0, 0))
+    # out = Image.new("RGB", (x, y), (0, 0, 0))
 
     # get a font
-    fontsize = int(x/2)
+    # fontsize = int(x/2)
+    fontsize = int(x/5)
+    # fontsize = int(x/6)
+
     fnt = ImageFont.truetype(filename, fontsize)
     print(filename)
     # get a drawing context
     d = ImageDraw.Draw(out)
 
     # draw multiline text
-    xoffset = (x/8) + (x/16)
-    yoffset = y/8
+    # xoffset = (x/8) + (x/16)
+    xoffset = (x/16)
+
+    # yoffset = y/8
+    yoffset = y/3
 
     # text black
-    #d.multiline_text((xoffset, yoffset), "$ make …", font=fnt, fill=(0, 0, 0))
+    d.multiline_text((xoffset, yoffset), text, font=fnt, fill=(0, 0, 0))
 
     # text white
-    d.multiline_text((xoffset, yoffset), "$ make …", font=fnt, fill=(255, 255, 255))
+    # d.multiline_text((xoffset, yoffset), text, font=fnt, fill=(255, 255, 255))
 
     fontname = filename.split(".")[0]  # Remove file extension
     out.save("logo.png", "PNG")
